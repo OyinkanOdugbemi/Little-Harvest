ab74efde55b9f34b8df7074d0a92a974e917e610
Oyinkansola Odugbemi <160362244+OyinkanOdugbemi@users.noreply.github.com>
2026-08-14 22:18:37 +0100

    Add files via upload

--- FarmPlot.hpp
+++ FarmPlot.hpp
@@ -2,10 +2,16 @@
 
 #include <SFML/Graphics.hpp>
 
+enum class CropType
+{
+    Carrot,
+    Strawberry
+};
+
 class FarmPlot
 {
     public:
-        FarmPlot(float x, float y);
+        FarmPlot(float x, float y, CropType cropType);
         void draw(sf::RenderWindow& window);
         void plant();
         void water();
@@ -19,6 +25,7 @@ class FarmPlot
     
     private:
         sf::RectangleShape soil;
+        CropType cropType;
         int growthStage = 0;
         bool watered = false;
 
